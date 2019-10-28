2c215e85a4ca955fcbf0107e6c99b14fffb623d0
richkiputra <richkiputra@gmail.com>
2019-10-28 16:25:45 +0700

    update Contact Fragment

--- app/src/main/java/com/example/consumerestapi/activity/adapter/ContactsAdapter.kt
+++ app/src/main/java/com/example/consumerestapi/activity/adapter/ContactsAdapter.kt
@@ -1,9 +1,14 @@
 package com.example.consumerestapi.activity.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.consumerestapi.R
 import com.example.consumerestapi.activity.data.ContactData
+import kotlinx.android.synthetic.main.row_item.view.*
 
 class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
 
@@ -11,16 +16,20 @@ class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
     var arrayList: ArrayList<ContactData> = ArrayList()
 
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        fun bindata(contactData: ContactData){
+        fun binData(contactData: ContactData){
 
+            Glide.with(view).load(contactData.image_url).apply(RequestOptions().circleCrop()).into(view.ivPhoto)
+
+            view.tvNameContact.text = contactData.name
+            view.tvPhoneContact.text = contactData.phone
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false )
 
-
-        return ViewHolder()
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -28,6 +37,6 @@ class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ContactsAdapter.ViewHolder, position: Int) {
-       holder.binData(arrayList[postition])
+       holder.binData(arrayList[position])
     }
 }
--- app/src/main/java/com/example/consumerestapi/activity/fragment/ContactFragment.kt
+++ app/src/main/java/com/example/consumerestapi/activity/fragment/ContactFragment.kt
@@ -7,15 +7,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.consumerestapi.R
 import com.example.consumerestapi.activity.DetailActivity
+import com.example.consumerestapi.activity.adapter.ContactsAdapter
 import kotlinx.android.synthetic.main.fragment_contact.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ContactFragment : Fragment() {
+
+    val contactsAdapter = ContactsAdapter()
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_contact, container, false)
@@ -24,9 +30,14 @@ class ContactFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-
+        initRecyclerView()
         initEvent()
 
+    }
+
+    private fun initRecyclerView() {
+        val linearLayoutManager = LinearLayoutManager(activity!!)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
     }
 
     private fun initEvent() {
